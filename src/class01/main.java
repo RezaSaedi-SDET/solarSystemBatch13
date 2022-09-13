52fc89e55879f10826bd63a1789f709e298ac70f
Reza Saedi <mreza.saedi@gmail.com>
2022-09-12 20:22:32 -0700

    Solar system ready to be deployed

--- src/class01/main.java
+++ src/class01/main.java
@@ -2,6 +2,11 @@ package class01;
 
 public class main {
     public static void main(String[] args) {
+        solarSystem inception=new solarSystem();
+        inception.sun=1;
+        inception.moon=4;
+        inception.planets=2;
+        System.out.println("the basic structure is ready with ");
 
     }
 }
